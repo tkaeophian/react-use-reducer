332f7e47a0c222deca2f84a8d1381687d620d74e
tee <tkaeophian@gmail.com>
2023-02-15 15:04:46 +0000

    simplify actions

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import './App.css';
 
 import { useReducer } from 'react';
 
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement',
+}
+
 function reducer(state, action) {
   switch(action.type) {
-    case 'increment':
+    case ACTIONS.INCREMENT:
       return { count: state.count + 1 }
-    case 'decrement':
+    case ACTIONS.DECREMENT:
       return { count: state.count - 1 }    
     default:
       return state
@@ -17,11 +22,11 @@ function App() {
   const [state, dispatch] = useReducer(reducer, { count : 0 })
   
   function increment() {
-    dispatch({ type: 'increment' })
+    dispatch({ type: ACTIONS.INCREMENT })
   }
 
   function decrement() {
-    dispatch({ type: 'decrement' })
+    dispatch({ type: ACTIONS.DECREMENT })
   }
 
   return (
