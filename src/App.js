97b94f2ff3d8899b21d363d4c3017f0bf3ff00d3
tee <tkaeophian@gmail.com>
2023-02-15 15:03:28 +0000

    add simple use reducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import './App.css';
 
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'increment':
+      return { count: state.count + 1 }
+    case 'decrement':
+      return { count: state.count - 1 }    
+    default:
+      return state
+  }
+}
 
 function App() {
-
-  const [count, setCount] = useState(0)
-
+  const [state, dispatch] = useReducer(reducer, { count : 0 })
+  
   function increment() {
-    setCount(prevCount => prevCount + 1)
+    dispatch({ type: 'increment' })
   }
 
   function decrement() {
-    setCount(prevCount => prevCount - 1)
+    dispatch({ type: 'decrement' })
   }
 
   return (
     <div className='App'>
       <button className='btn' onClick={decrement}>-</button>
-        <span className='number'>{count}</span>
+        <span className='number'>{state.count}</span>
       <button className='btn' onClick={increment}>+</button>
    </div>
   );
